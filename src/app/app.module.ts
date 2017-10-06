1eaad083fd686c6e77542cacdf22ceeb99c4e829
infamousbazooka <magnusfernandes1295@gmail.com>
2017-10-06 17:04:23 +0530

    Reactive forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
-import { DataService } from './data.service'
+import { DataService } from './data.service';
+import { NewComponent } from './new/new.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    NewComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ReactiveFormsModule
   ],
   providers: [ DataService],
   bootstrap: [AppComponent]
